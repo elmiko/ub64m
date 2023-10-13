a7cf64e63391db9bf613194547e684b70bcd3c8f
michael mccune <msm@opbstudios.com>
2023-10-13 16:30:10 -0400

    add basic file load and print

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+// Copyright (C) 2023 michael mccune
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+use anyhow::{anyhow, Result};
+use clap::Parser;
+use std::fs;
+use std::path::Path;
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None )]
+struct Cli {
+    /// The YAML manifest filename to decode
+    filename: String,
+}
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+
+    let manifest_path: &Path = cli.filename.as_ref();
+    if !manifest_path.is_file() {
+        return Err(anyhow!("Path is not a file {}", manifest_path.display()));
+    }
+
+    let raw = fs::read_to_string(manifest_path)?;
+
+    println!("{}", raw);
+
+    Ok(())
 }
